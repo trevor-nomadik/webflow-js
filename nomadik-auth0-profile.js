2b08c92c253a67ee41d16779a33bfdf30dca4736
trevor <trevor.sorrells45@gmail.com>
2024-02-09 12:35:56 -0600

    reverted auth0 client

--- nomadik-auth0-profile.js
+++ nomadik-auth0-profile.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initialize the Auth0 client
     const auth0 = new Auth0Client({
       domain: 'dev-oxu1mjqlqdkb8h1l.us.auth0.com',
-      client_id: 'SIdFpei8f7cUhfHTk9QPWU8SEgizV6np',
+      client_id: 'LNtA6Fn3VpVyKikh3CcxrF9xqRN93hyy',
       redirect_uri: window.location.origin,
       scope: 'openid profile email',
       cacheLocation: 'localstorage',
